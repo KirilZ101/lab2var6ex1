39922e3fe87f6c652f1dedd2e1007c9aed1034fb
KirilZ101 <152603999+KirilZ101@users.noreply.github.com>
2024-02-24 16:05:43 +0300

    Initial Commit

---
+++ lab2var6ex1/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  lab2var6ex1
+//
+//  Created by Кирилл Заболотний on 24.02.2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
